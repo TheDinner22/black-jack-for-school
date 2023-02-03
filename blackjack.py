c78ba0249d7a805e3956604d23d15e4d29c1ff03
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-02-03 17:25:15 -0500

    added some comments on reqs for program

---
+++ blackjack.py
@@ -0,0 +1,43 @@
+# Joe Goodman
+# 02/03/23
+# black jack card game
+
+"""
+notes on project (in markdown format)
+
+# starting the program
+
+1. when the program is started, "START GAME #1" should be printed to the console
+2. the player should automatically be dealt their first card
+
+> each game should start with "START GAME #1" printed to the console 
+> NOTE: adjust game number as needed
+
+# how to "randomly " draw cards
+
+1. Generate a number between 1 and 13 (inclusive)
+2. there is a mapping between numbers and cards
+    - 1 is an ace
+    - 2-10 (inclusive) are the numbered cards
+    - 11 is the jack
+    - 12 is the queen
+    - 13 is the king
+
+# card point values
+
+Same as in regular blackjack except that aces are only ever worth one point.
+
+# announcing dealt cards WIP
+
+Whenever a card is dealt, print the cards name and the total number of points in that players hand
+
+For example, when delt a king the stdout would read: "Your card is a KING!"
+For example, when delt a number card (2-10) with number, n, the stdout would read: "Your card is a n!"
+
+# printing the menu after a card is dealt
+
+todo!()
+
+
+
+"""
