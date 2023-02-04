b7694788b95db50fd6e80c33c85a80d3afa41585
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-02-04 09:48:11 -0500

    start of main and helper IO functions

--- blackjack.py
+++ blackjack.py
@@ -25,6 +25,7 @@ class Card:
         self.point_value = number if number <= 10 else 10
 
         # map the number to a card name
+        # 1 is an ACE
         # for numbers >1 but <=10, the name and the number are the same
         # 11, 12, and 13 are mapped to JACK, QUEEN, and KING
         name = None
@@ -46,7 +47,74 @@ class Card:
         self.name = name
 
 def main():
-    pass
+    # global values to track
+    playing = True
+
+    # number of games played
+    game_number = 1
+
+    # number of player wins
+    player_wins = 0
+
+    # number of dealer wins
+    dealer_wins = 0
+    
+    # number of ties
+    ties = 0
+
+    while playing:
+        # print the game number and menu
+        print_game_number(game_number)
+        print_menu()
+
+        # ask the user which option they would like to select
+        user_input = get_checked_user_input("Choose an option: ")
+
+        # determine what to do based on the users input
+        if user_input == 1:
+            # give the player another card
+            pass
+        elif user_input == 2:
+            # dealers turn and decide winner
+            pass
+        elif user_input == 3:
+            # print game stats
+            pass
+        elif user_input == 4:
+            playing = False
+        else:
+            error("unreachable!")
+
+# get input from the user and ensure that it is valid
+# this function will reject any input that is not "1", "2", "3", or "4"
+# it will loop forever or until it gets valid input
+def get_checked_user_input(msg):
+    # a list of all valid inputs
+    valid_inputs = ["1", "2", "3", "4"]
+    while True:
+        # get input from the user
+        user_input = input(msg)
+
+        # if it is valid, return the input as an int
+        if user_input in valid_inputs:
+            return int(user_input)
+
+        # otherwise, let the user know to try again
+        else:
+            print("Invalid input!") 
+            print("Please enter an integer value between 1 and 4.") 
+
+# print the game number
+def print_game_number(game_number):
+    print(f"START GAME #{game_number}")
+
+# print the menu
+def print_menu():
+    print("1. Get another card") 
+    print("2. Hold hand") 
+    print("3. Print statistics") 
+    print("4. Exit")
 
 if __name__ == "__main__":
     main()
+
