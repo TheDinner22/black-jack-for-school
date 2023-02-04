c94914bdb0d357270e1d18a8f0300a1e53c31f9d
Joe Goodman <79279956+TheDinner22@users.noreply.github.com>
2023-02-04 08:54:10 -0500

    learning how to use the rng

--- blackjack.py
+++ blackjack.py
@@ -2,3 +2,27 @@
 # 02/03/23
 # black jack card game
 
+"""
+questions
+1. does import really need to be first line
+"""
+
+from p1_random import P1Random
+
+def main():
+    rng = P1Random()
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+    print(rng.next_int(14))
+
+    pass
+
+if __name__ == "__main__":
+    main()
